d2f5a8da8cdfa985ed572ef7f8ea223ec9874883
tetiana <tetianayaremko@gmail.com>
2020-06-11 13:33:56 +0200

    DishDetails changed to a functional component

--- src/components/DishDetail.jsx
+++ src/components/DishDetail.jsx
@@ -1,54 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card, CardBody, CardSubtitle, CardText, CardTitle } from "reactstrap";
 import items from "../data/menu.json";
 import DisplayDishComments from "./DisplayDishComments.jsx";
 
-class DishDetail extends React.Component {
-  constructor(params) {
-    super(params);
+function DishDetail(props) {
+  const [dish, setDish] = useState(null);
 
-    this.state = {
-      dish: null
-    };
-  }
+  useEffect(() => {
+    let dishId = props.match.params.dishId;
 
-  render() {
-    return (
-      <>
-        {this.state.dish && (
-          <>
-            <div className="row">
-              <div className="col-md-3">
-                <img src={"/" + this.state.dish.image} className="img-fluid" alt={this.state.dish.description} />
-              </div>
-              <div className="col-md-9">
-                <Card>
-                  <CardBody>
-                    <CardTitle>{this.state.dish.name}</CardTitle>
-                    <CardSubtitle>{this.state.dish.label}</CardSubtitle>
-                    <CardText>{this.state.dish.description}</CardText>
-                  </CardBody>
-                </Card>
-              </div>
+    let temp = items.find((dish) => dish.id.toString() === dishId);
+    setDish(temp);
+  }, []);
+
+  return (
+    <>
+      {dish && (
+        <>
+          <div className="row">
+            <div className="col-md-3">
+              <img
+                src={"/" + dish.image}
+                className="img-fluid"
+                alt={dish.description}
+              />
             </div>
-            <div className="row">
-              <DisplayDishComments selectedDish={this.state.dish} />
+            <div className="col-md-9">
+              <Card>
+                <CardBody>
+                  <CardTitle>{dish.name}</CardTitle>
+                  <CardSubtitle>{dish.label}</CardSubtitle>
+                  <CardText>{dish.description}</CardText>
+                </CardBody>
+              </Card>
             </div>
-          </>
-        )}
-        {!this.state.dish && <h1>Loading...</h1>}
-      </>
-    );
-  }
-
-  componentDidMount() {
-    var dishId = this.props.match.params.dishId;
-
-    var dish = items.find(dish => dish.id.toString() === dishId);
-    this.setState({
-      dish: dish
-    });
-  }
+          </div>
+          <div className="row">
+            <DisplayDishComments selectedDish={dish} />
+          </div>
+        </>
+      )}
+      {!dish && <h1>Loading...</h1>}
+    </>
+  );
 }
 
 export default DishDetail;
