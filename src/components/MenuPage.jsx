e171c2be340e41089748a54679b354d71d5f0dfe
tetiana <tetianayaremko@gmail.com>
2020-06-11 13:59:33 +0200

    short-circuit operator in Menu component

--- src/components/MenuPage.jsx
+++ src/components/MenuPage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import items from "../data/menu.json";
 import SingleDish from "./SingleDish.jsx";
+import { Alert } from "reactstrap";
 
 class MenuPage extends React.Component {
   state = {};
   render() {
-    return <>{items && items.map(dish => <SingleDish dish={dish} key={dish.id} />)}</>;
+    return (
+      <>
+        {items && items.map((dish) => <SingleDish dish={dish} key={dish.id} />)}
+        {!items && <Alert color="secondary">No dishes</Alert>}
+      </>
+    );
   }
 }
 
